c0b6e8bf389f68423c4ffd68e24b908557a62575
HEXIANG WANG <z5345311@ad.unsw.edu.au>
2023-11-01 20:55:53 +0800

    img minor

--- src/components/small_components/TradeRequestCard.jsx
+++ src/components/small_components/TradeRequestCard.jsx
@@ -17,15 +17,30 @@
 function TradeRequestCard({ trade, myName, onConfirmClick, onDeclineClick }) {
     return (
         <div className="trade-request-card">
-            <img src="./assets/img/default-avatar.jpg" alt="item image"></img>
-            <h3 className="trade-request-card-set">{trade.collectableSet}</h3>
-            <h3 className="trade-request-card-name">{trade.collectableName}</h3>
-            <p className="trade-request-card-price">{trade.price}</p>
-            <p className="trade-request-card-selling-or-buying">{trade.sellingOrBuying}</p>
+            {myName === trade.requesterName ? (
+                <div className="outgoing-request-card">
+                    <h2 className="other-name">My {trade.sellingOrBuying === "SELLING" ? "buying" : "selling"} request to {trade.ownerName}</h2>
+                    <img src="/assets/img/default-avatar.jpg" alt="item image"></img>
+                    <h3 className="trade-request-card-set">{trade.collectableSet}</h3>
+                    <h3 className="trade-request-card-name">{trade.collectableName}</h3>
+                    <p className="trade-request-card-price">{trade.price}</p>
 
-            <button className="trade-request-card-confirm-btn" onClick={() => onConfirmClick(trade.exchangeId)}>Confirm</button>
-            <button className="trade-request-card-decline-btn" onClick={() => onDeclineClick(trade.exchangeId)}>Decline</button>
-        </div>
+                    <button className="trade-request-card-decline-btn" onClick={() => onDeclineClick(trade.exchangeId)}>Cancel</button>
+                </div>
+            ) : (
+                <div className="incoming-request-card">
+                    <h2 className="other-name">{trade.requesterName}'s {trade.sellingOrBuying === "SELLING" ? "buying" : "selling"} request to me</h2>
+                    <img src="/assets/img/default-avatar.jpg" alt="item image"></img>
+                    <h3 className="trade-request-card-set">{trade.collectableSet}</h3>
+                    <h3 className="trade-request-card-name">{trade.collectableName}</h3>
+                    <p className="trade-request-card-price">{trade.price}</p>
+
+                    <button className="trade-request-card-confirm-btn" onClick={() => onConfirmClick(trade.exchangeId)}>Confirm</button>
+                    <button className="trade-request-card-decline-btn" onClick={() => onDeclineClick(trade.exchangeId)}>Decline</button>
+                </div>
+            )
+            }
+        </div >
     )
 }
 
