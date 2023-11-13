4a9474300ab5f2149a6c1ea7dca465b39d196b1c
EricaHu <xihu4221@uni.sydney.edu.au>
2023-11-13 16:42:02 +0800

    analytics

--- src/components/small_components/Analytics.jsx
+++ src/components/small_components/Analytics.jsx
@@ -1,15 +1,11 @@
-// 统计图表
-// 30天内用户交易量排行前十的用户名及交易数量（排行榜
-// 30天内总交易数量
-// 30天内每个set的总交易量（柱状图
-// 现有set的exchange数量（柱状图
-
 import React from "react";
+import Plot from "react-plotly.js";
+import starIcon from "../../static/star.png";
 
 function Analytics() {
     const [analytics, setAnalytics] = React.useState(null);
     React.useEffect(() => {
-        fetch("/manage/managerAuthority")
+        fetch("/api/manage/analytics")
             .then((response) => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
@@ -18,12 +14,132 @@ function Analytics() {
             })
             .then((data) => {
                 setAnalytics(data);
-            })
+            });
     }, []);
-    
-    return ( <section className="analytics-container">
-        
-    </section> );
+
+    function createBarChart(data, title) {
+        return (
+            <Plot
+                data={[
+                    {
+                        x: data.map((item) => item.collectableSet),
+                        y: data.map((item) => item.count),
+                        type: "bar",
+                        marker: { color: "#2ca02c" },
+                    },
+                ]}
+                layout={{
+                    autosize: true,
+                    title: {
+                        text: title,
+                        font: {
+                            family: "Arial, sans-serif",
+                            size: 24,
+                            weight: "bold",
+                        },
+                    },
+                    xaxis: {
+                        title: {
+                            text: "Collectable Set",
+                            font: {
+                                family: "Arial, sans-serif",
+                                size: 18,
+                                color: "#7f7f7f",
+                            },
+                        },
+                        tickfont: {
+                            family: "Arial, sans-serif",
+                            size: 14,
+                            color: "#7f7f7f",
+                        },
+                    },
+                    yaxis: {
+                        title: {
+                            text: "Count",
+                            font: {
+                                family: "Arial, sans-serif",
+                                size: 18,
+                                color: "#7f7f7f",
+                            },
+                        },
+                        tickfont: {
+                            family: "Arial, sans-serif",
+                            size: 14,
+                            color: "#7f7f7f",
+                        },
+                    },
+                }}
+                useResizeHandler={true}
+                style={{ width: "100%", height: "100%" }}
+            />
+        );
+    }
+
+    // Trader name and trade amout of top 10 trader in one month
+    const traderRank = analytics && analytics.traderRank && (
+        <section className="text-container">
+            <h2>Top traders in 30 days:</h2>
+            <ol>
+                {analytics.traderRank.map((trader, index) => (
+                    <li key={index}>
+                        {index < 3 && (
+                            <span className="top-trader-icon">🏆</span>
+                        )}
+                        <strong>{trader.trader}:</strong> {trader.tradeCount}{" "}
+                        {trader.tradeCount > 1 ? "trades" : "trade"}
+                    </li>
+                ))}
+            </ol>
+        </section>
+    );
+
+    // Total trade amount in one month
+    const tradeAmount = analytics && analytics.tradeAmount && (
+        <section className="text-container">
+            <h2>Total Trade Amount in 30 days:</h2>
+            <h1>{analytics.tradeAmount}</h1>
+        </section>
+    );
+
+    // Trade amount of each set in one month (bar chart)
+    const setRank = analytics && analytics.setRank && (
+        <section className="chart-container">
+            {createBarChart(
+                analytics.setRank,
+                "Trade Amount of Each Set in 30 days:"
+            )}
+        </section>
+    );
+
+    // Current exchange in plaza of each set (bar chart)
+    const exchangeRank = analytics && analytics.exchangeRank && (
+        <section className="chart-container">
+            {createBarChart(
+                analytics.exchangeRank,
+                "Current Exchange in Plaza of Each Set:"
+            )}
+        </section>
+    );
+
+    return (
+        <section className="analytics-container">
+            {analytics ? (
+                <>
+                <h2 className="section-title">Analytics</h2>
+                    <div>
+                        {traderRank}
+                        {tradeAmount}
+                    </div>
+                    <div>
+                        {setRank}
+                        {exchangeRank}
+                    </div>
+                </>
+            ) : (
+                <p>Loading analytics...</p>
+            )}
+        </section>
+    );
 }
 
 export default Analytics;
