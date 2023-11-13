2423545d033e9df14aeef040e80600273a00d7ff
EricaHu <xihu4221@uni.sydney.edu.au>
2023-11-13 18:05:27 +0800

    feedback page

--- src/components/MAFeedbackPage.jsx
+++ src/components/MAFeedbackPage.jsx
@@ -1,13 +1,71 @@
 import FeedbackCard from "./small_components/FeedbackCard";
 import "../css/MAFeedbackPage.scss";
 import MANavbar from "./small_components/MANavbar";
+import React from "react";
 
-// 所有账号都有这个界面
-
+// Every account type have this page
 function MAFeedbackPage() {
+    const [feedbacks, setFeedbacks] = React.useState(null);
+
+    // load feedbacks
+    React.useEffect(() => {
+        fetch("/api/manage/feedback")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setFeedbacks(data);
+            })
+            .catch((error) => {
+                console.error("Fetch feedback error:", error);
+            });
+    }, []);
+
+    function handleReadClick(id) {
+        fetch(`/api/manage/feedback/check?id=${id}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.text();
+            })
+            .then((data) => {
+                if (data === "Success.") {
+                    setFeedbacks(
+                        feedbacks.filter((feedback) => feedback.id !== id)
+                    );
+                }
+            })
+            .catch((error) => {
+                console.error("Fetch error:", error);
+            });
+    }
+
     return (
         <>
             <MANavbar />
+
+            <section className="body-container">
+                <h1>User Feedback</h1>
+                {feedbacks !== null && feedbacks.length > 0 ? (
+                    <div className="feedback-card-container">
+                        {feedbacks.map((feedback) => (
+                            <FeedbackCard
+                                key={feedback.id}
+                                onReadClick={handleReadClick}
+                                feedback={feedback}
+                            />
+                        ))}
+                    </div>
+                ) : feedbacks !== null ? (
+                    <div>No feedback yet.</div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </section>
         </>
     );
 }
--- src/components/small_components/FeedbackCard.jsx
+++ src/components/small_components/FeedbackCard.jsx
@@ -1,12 +1,34 @@
-
-// feedback card, {username: "", feedback: "", updateDate: ""} 
-// Have a checkbox 
+// feedback card, {username: "", feedback: "", updateDate: ""}
+// Have a checkbox
 // POST id
 // /api/manage/feedback/check?id={}
 
-function FeedbackCard() {
-    return ( <>
-    </> );
+function FeedbackCard({ feedback, onReadClick }) {
+    return (
+        <div className="feedback-card">
+            <h3>From {feedback.username}:</h3>
+            <p>{feedback.feedback}</p>
+            <h3>
+                {new Date(feedback.updateDate)
+                    .getHours()
+                    .toString()
+                    .padStart(2, "0")}
+                :
+                {new Date(feedback.updateDate)
+                    .getMinutes()
+                    .toString()
+                    .padStart(2, "0")}{" "}
+                {new Date(feedback.updateDate).getDate()}/
+                {(new Date(feedback.updateDate).getMonth() + 1)
+                    .toString()
+                    .padStart(2, "0")}
+                /{new Date(feedback.updateDate).getFullYear()}
+            </h3>
+            <button onClick={() => onReadClick(feedback.id)}>
+                Mark as read
+            </button>
+        </div>
+    );
 }
 
 export default FeedbackCard;
